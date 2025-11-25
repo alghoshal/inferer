a4cb2dca80dedd8f8a35b289f4df4fe87305dd67
algo <alghoshal@gmail.com>
2025-11-26 01:15:40 +0530

    Shap intg

--- src/main/python/keras_io_examples_nlp/TextClassificationTorchShap.py
+++ src/main/python/keras_io_examples_nlp/TextClassificationTorchShap.py
@@ -1,6 +1,7 @@
 from TextClassificationTorchUtilities import *
 import shap
 from shap import maskers
+import random
 
 """
 Perform Inference using a saved Keras PyTorch TextClassification model and its vocabulary (vocab)
@@ -39,7 +40,7 @@ def predict(vectorizedData):
 
 # TODO: Fix me!
 def maskIt(mask,text):
-    return ["This is great one to watch. ",
+    maskedText = ["This is great one to watch. ",
              "This is <unk> one to watch. ",
             "<unk> is great one to watch. ",
             "<unk> <unk> <unk> one to watch. ",
@@ -47,6 +48,8 @@ def maskIt(mask,text):
             "<unk> <unk> <unk> <unk> <unk> watch. ",
             "<unk> <unk> <unk> <unk> <unk> <unk> "
             ]
+    random.shuffle(maskedText)
+    return maskedText[:3]
 counter=0
 def masker(mask, *args):
     global counter
@@ -58,5 +61,5 @@ explainer = shap.Explainer(predict,masker=masker)
 shap_values = explainer(one_batch_text[:1], batch_size=batch_size, max_evals=5)
 print(shap_values[0])
 
-#shapPlot=shap.plots.text(shap_values[0],display=False)
-#print(shapPlot)
+shapPlot=shap.plots.text(shap_values[0],display=False)
+print(shapPlot)
