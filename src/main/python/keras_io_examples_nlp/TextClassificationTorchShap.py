eabcd081918aaccf62cbce0a1e1f7c866f2509e1
algo <alghoshal@gmail.com>
2025-11-26 20:21:59 +0530

    Shap intg

--- src/main/python/keras_io_examples_nlp/TextClassificationTorchShap.py
+++ src/main/python/keras_io_examples_nlp/TextClassificationTorchShap.py
@@ -3,8 +3,10 @@ import shap
 from shap import maskers
 import random
 
+
 """
-Perform Inference using a saved Keras PyTorch TextClassification model and its vocabulary (vocab)
+View Shap values of a trained Keras PyTorch TextClassification 
+(Model: ./text_classification_torch.py)
 
 Imdb data source: https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz
 """
@@ -14,52 +16,63 @@ Imdb data source: https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.g
 """
     
 imdb_files_dir="tests/aclImdbTen" # Ten files 
-batch_size=1
+batch_size=2
+SHAP_PLOT_OUTPUT_FILE=SAVE_TO_DIR+'TextClassificationShapPlot.html'
 # Load Imdb data
 #raw_test_ds = text_dataset_from_directory(imdb_files_dir+"/test", batch_size=batch_size, format="grain")
 
-one_batch_text=[("This is great one to watch.",)]
+one_batch_text=[(["This is a great one to watch.","What a long drawn boring affair to the end credits."],)]
 # Fetch one batch data from test_ds
 #for text_batch, label_batch in raw_test_ds:  
 #    one_batch_text.append(text_batch)
 
 one_batch_text=np.array(one_batch_text)
-   
+
 print("Load Vocab")
 vocab=loadVocabFromFile(SAVE_TO_DIR+"TextClassificationVocab.pkl")
 
 print("Load Model")
 kerasModel=keras.models.load_model(SAVE_TO_DIR+'TextClassificationTorchModel.keras')
 
-def predict(vectorizedData):
-    print("Predict called ")
-    vectorzd_text_batch,one_batch_labels = vectorize_text([vectorizedData,[]])
+'''
+Input data has words masked (at random) using the specified mask(SPECIAL_TOKEN_UNK). 
+E.g.: "<unk> is a great one to watch.", "This is a <unk> one to watch.",...
+
+Snap uses the output to evaluate the impact of the specific masked word (token).
+'''
+def predict(inputData):
+    print("Predict called: "+inputData)
+    vectorzd_text_batch,one_batch_labels = vectorize_text([inputData,[]])
     outputs = kerasModel(vectorzd_text_batch).detach().numpy().flatten()
     print(outputs)
     return outputs
 
-# TODO: Fix me!
-def maskIt(mask,text):
-    maskedText = ["This is great one to watch. ",
-             "This is <unk> one to watch. ",
-            "<unk> is great one to watch. ",
-            "<unk> <unk> <unk> one to watch. ",
-            "<unk> <unk> <unk> <unk> to watch. ",
-            "<unk> <unk> <unk> <unk> <unk> watch. ",
-            "<unk> <unk> <unk> <unk> <unk> <unk> "
-            ]
-    random.shuffle(maskedText)
-    return maskedText[:3]
-counter=0
-def masker(mask, *args):
-    global counter
-    counter+=1
-    print("Masker called")
-    return maskIt(mask,args[0])
-    
-explainer = shap.Explainer(predict,masker=masker)
-shap_values = explainer(one_batch_text[:1], batch_size=batch_size, max_evals=5)
-print(shap_values[0])
+def custom_tokenizer(s, return_offsets_mapping=True):
+    """Custom tokenizers conform to a subset of the transformers API."""
+    pos = 0
+    offset_ranges = []
+    input_ids = []
+    for m in re.finditer(r"\W", s):
+        start, end = m.span(0)
+        offset_ranges.append((pos, start))
+        input_ids.append(s[pos:start])
+        pos = end
+    if pos != len(s):
+        offset_ranges.append((pos, len(s)))
+        input_ids.append(s[pos:])
+    out = {}
+    out["input_ids"] = input_ids
+    if return_offsets_mapping:
+        out["offset_mapping"] = offset_ranges
+    return out
 
-shapPlot=shap.plots.text(shap_values[0],display=False)
-print(shapPlot)
+masker = maskers.Text(custom_tokenizer, mask_token=SPECIAL_TOKEN_UNK)
+explainer = shap.Explainer(predict,masker=masker)
+shap_values = explainer(one_batch_text[:2].flatten(), batch_size=batch_size, max_evals=5)
+print(shap_values)
+
+shapPlot=shap.plots.text(shap_values,display=False)
+
+# Save shapPlot
+with open(SHAP_PLOT_OUTPUT_FILE, "w") as f:
+    f.write(shapPlot)
